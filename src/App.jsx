a07c306460f5e55e0ed78064190bf0b5c476301e
Hana Maruškevičová <hanka.maruskevicova@gmail.com>
2024-02-08 13:08:11 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#290075] to-[#3e00b1]`,
+
+}
+
+function App() {
+
+  //create todo
+  //read todo
+  //update todo
+  //delete todo
+  
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h1 className={style.heading}>Todo App</h1>
+        <form className={style.form}>
+          <input type="text" placeholder="Add Todo" className={style.input} />
+          <button type="submit" className={style.button}>Add</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App;
